14de90d9c3919f64f5518d39c25cff041eeddca0
japanesebonobo <tomi119119@gmail.com>
2019-10-01 12:11:03 +0900

    修正

--- Bunjho_scraper_detail_test.py
+++ Bunjho_scraper_detail_test.py
@@ -8,11 +8,17 @@ async def main():
     page = await browser.newPage()
 
     linkData = pd.read_csv('/Users/yoshitomiyuuta/Bunjho_web_develop/linkData.csv',usecols=[1])
-    print(linkData)
+    scoreData = []
 
     for i, row in linkData.iterrows():
-        await page.
-        goto(str(row[0]))
+        await page.goto(str(row[0]))
+
+        for td in await page.querySelectorAll('body > div > table > tbody > tr > td > table > tbody > tr > td'):
+            time.sleep(1)
+            score = await page.evaluate('(e) => e.innerText',td)
+            score = score.strip()
+            scoreData.append(score)
+            print(score)
         time.sleep(1)
 
     await browser.close()
