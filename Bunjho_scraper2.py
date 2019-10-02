92452a080a64110990eee665d0aa51b329749a4a
japanesebonobo <tomi119119@gmail.com>
2019-10-02 12:36:51 +0900

    詳細ページのスクレイピング追加

--- Bunjho_scraper2.py
+++ Bunjho_scraper2.py
@@ -49,6 +49,23 @@ async def main():
     subjectData.to_csv('/Users/yoshitomiyuuta/Bunjho_web_develop/subjectData.csv')
     linkData.to_csv('/Users/yoshitomiyuuta/Bunjho_web_develop/linkData.csv')
 
+    scoreData = []
+
+    for i, row in linkData.iterrows():
+        await page.goto(str(row[0]))
+
+        for td in await page.querySelectorAll('body > div > table > tbody > tr > td > table > tbody > tr > td'):
+            time.sleep(1)
+            score = await page.evaluate('(e) => e.innerText',td)
+            score = score.strip()
+            scoreData.append(score)
+            print(score)
+        time.sleep(0.1)
+
+    scoreData = pd.DataFrame(scoreData)
+    print(scoreData)
+    scoreData.to_csv('/Users/yoshitomiyuuta/Bunjho_web_develop/scoreData.csv')
+
     await browser.close()
 
 if __name__=='__main__':
