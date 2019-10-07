d9f3ebc584cb6d69e36028046ef6185bd06d1fdb
japanesebonobo <tomi119119@gmail.com>
2019-10-07 15:36:12 +0900

    MysqlとDjangoを接続し、AllSubjectDataテーブルを登録

---
+++ mysite/bunjho_web_database/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+class Bunjho_web_database(models.Model):
+    subjectData_index = models.IntegerField()
+    subjectNo = models.TextField()
+    faculty = models.TextField()
+    subjectName = models.TextField()
+    teacher = models.TextField()
+    place = models.TextField()
+    units = models.TextField()
+    scoreData_index = models.IntegerField()
+    member = models.IntegerField()
+    A = models.FloatField()
+    B = models.FloatField()
+    C = models.FloatField()
+    D = models.FloatField()
+    F = models.FloatField()
+    other = models.IntegerField()
+    averageGPA = models.FloatField()
+    linkData_index = models.IntegerField()
+    link = models.TextField()
+
+class Allsubjectdata(models.Model):
+    subjectdata_index = models.IntegerField(db_column='subjectData_index', blank=True, null=True, primary_key=True)  # Field name made lowercase.
+    subjectno = models.TextField(db_column='subjectNo', blank=True, null=True)  # Field name made lowercase.
+    faculty = models.TextField(blank=True, null=True)
+    subjectname = models.TextField(db_column='subjectName', blank=True, null=True)  # Field name made lowercase.
+    teacher = models.TextField(blank=True, null=True)
+    place = models.TextField(blank=True, null=True)
+    units = models.TextField(blank=True, null=True)
+    scoredata_index = models.IntegerField(db_column='scoreData_index', blank=True, null=True)  # Field name made lowercase.
+    member = models.IntegerField(blank=True, null=True)
+    a = models.FloatField(db_column='A', blank=True, null=True)  # Field name made lowercase.
+    b = models.FloatField(db_column='B', blank=True, null=True)  # Field name made lowercase.
+    c = models.FloatField(db_column='C', blank=True, null=True)  # Field name made lowercase.
+    d = models.FloatField(db_column='D', blank=True, null=True)  # Field name made lowercase.
+    f = models.FloatField(db_column='F', blank=True, null=True)  # Field name made lowercase.
+    other = models.IntegerField(blank=True, null=True)
+    averagegpa = models.FloatField(db_column='averageGPA', blank=True, null=True)  # Field name made lowercase.
+    linkdata_index = models.IntegerField(db_column='linkData_index', blank=True, null=True)  # Field name made lowercase.
+    link = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'AllSubjectData'
--- mysite/manage.py
+++ mysite/manage.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 import pymysql
 
 pymysql.install_as_MySQLdb()
 
-
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bunjho_web.settings')
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,7 +16,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
