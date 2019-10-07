f61e512e814f84f44a3cbc901ab20d806c1d48fb
japanesebonobo <tomi119119@gmail.com>
2019-10-07 17:05:51 +0900

    管理画面を作成

--- mysite/bunjho_web_database/views.py
+++ mysite/bunjho_web_database/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Allsubjectdata
 
 # Create your views here.
+def index(request):
+    data = Allsubjectdata.objects.all()
+    params = {
+        'title': 'Bunjho_2018_AllSubjectData',
+        'message': '2019/10/7',
+        'data': data,
+    }
+    return render(request, '2018_subject_data/index.html', params)
