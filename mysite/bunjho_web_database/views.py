7822322d80650d34a3f9b0d3effb9fa574ef6724
japanesebonobo <tomi119119@gmail.com>
2019-10-11 14:56:25 +0900

    表示項目を変更、フォントサイズ拡大、中央揃え

--- mysite/bunjho_web_database/views.py
+++ mysite/bunjho_web_database/views.py
@@ -3,7 +3,7 @@ from .models import Allsubjectdata
 
 # Create your views here.
 def index_page(request):
-    data = Allsubjectdata.objects.all().order_by('subjectno')
+    data = Allsubjectdata.objects.all().values('subjectname','teacher','units','member','a','b','c','d','f','averagegpa','link').distinct().order_by('subjectno')
     params = {
         'title': 'Bunjho_2018_AllSubjectData',
         'message': '2019/10/7',
